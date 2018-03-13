9b78f25b98e698db1ae02086208e43a237db2b77
effu <cowdell@gmail.com>
2018-03-13 13:33:28 -0600

    IPAddressContainer component receiving data

--- ipaddress/src/IPAddressContainer.js
+++ ipaddress/src/IPAddressContainer.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 
+var xhr;
+
 class IPAddressContainer extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      ip_address: "..."
+    };
+
+    this.processRequest = this.processRequest.bind(this);
+  }
+
+  componentDidMount() {
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://ipinfo.io/json", true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange", this.processRequest, false);
+  }
+
+  processRequest() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+
+      this.setState({
+        ip_address: response.ip
+      });
+    }
+  }
+
   render() {
     return (
-      <p>Nothing yet!</p>
+      <div>{this.state.ip_address}</div>
     );
   }
-}
+};
 
 export default IPAddressContainer;
